a36a4049680c8c27924f25f198b59da5132574f3
Rich <tanakovr@yandex.ru>
2022-01-03 15:31:37 +0300

    Добавление settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,17 +1,28 @@
 import sys
 import pygame
+from settings import Settings
+
 
 def run_game():
     # Инифцилизирует игру и создает объект экрана
     pygame.init()
-    screen=pygame.display.set_mode((1200,700))
     pygame.display.set_caption("Alien battle")
-    # Запуск основного цикла 
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+
+    # Назначение цвета фона
+    bg_color = (90, 90, 90)
+    # Запуск основного цикла
     while True:
         # Отслеживание событий клавиатуры
         for event in pygame.event.get():
-            if event.type==pygame.QUIT:
+            if event.type == pygame.QUIT:
                 sys.exit()
-        # Отображение последнего прорисованного экрана 
+    #   При каждом проходе цикла перерисовывается экран
+        screen.fill(ai_settings.bg_color)
+        # Отображение последнего прорисованного экрана
         pygame.display.flip()
+
+
 run_game()
