45dd76701ffca62e1261f3b539e3c800d42cd45a
Rich <tanakovr@yandex.ru>
2022-01-03 16:09:41 +0300

    Рефакторинг

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,28 +1,21 @@
-import sys
 import pygame
 from settings import Settings
 from ship import Ship
-
+import game_functions as gf
 def run_game():
     # Инифцилизирует игру и создает объект экрана
     pygame.init()
     pygame.display.set_caption("Alien battle")
     ai_settings = Settings()
+    
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     ship=Ship(screen)
-
+    
     # Запуск основного цикла
     while True:
-        # Отслеживание событий клавиатуры
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
     #   При каждом проходе цикла перерисовывается экран
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        # Отображение последнего прорисованного экрана
-        pygame.display.flip()
-
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -1,1 +1,16 @@
-game_functions.py
+import sys
+import pygame
+
+
+def check_events():
+    # Отслеживание событий клавиатуры
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+
+def update_screen(ai_settings, screen, ship):
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+    # Отображение последнего прорисованного экрана
+    pygame.display.flip()
