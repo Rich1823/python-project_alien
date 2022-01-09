442e73ee4450964fe48b590dae8d8266f3306779
Rich <tanakovr@yandex.ru>
2022-01-10 00:59:32 +0300

    Добавлена кнопка запуска

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,32 +5,38 @@ from pygame.sprite import Group
 from alien import Alien
 import game_functions as gf
 from game_stats import GameStats
+from button import Button
+
 
 def run_game():
     # Инифцилизирует игру и создает объект экрана
     pygame.init()
     pygame.display.set_caption("Alien battle")
     ai_settings = Settings()
-    stats=GameStats(ai_settings)
+    stats = GameStats(ai_settings)
 
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     ship = Ship(ai_settings, screen)
+    # Создание кнопки Play
+    play_button = Button(ai_settings, screen, "Погнали!")
     # Создание группы для хранения пуль
     bullets = Group()
     aliens = Group()
     alien = Alien(ai_settings, screen)
     # создание пришельца
-    gf.create_fleet(ai_settings, screen,ship, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
     # Запуск основного цикла
     while True:
         #   При каждом проходе цикла перерисовывается экран
         gf.check_events(ai_settings, screen, ship, bullets)
         if stats.game_active:
             ship.update()
-            gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
-            gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
-            gf.update_screen(ai_settings, screen, ship, aliens, bullets)
-    
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen,stats, ship,
+                             aliens, bullets, play_button)
+
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -4,7 +4,6 @@ from bullet import Bullet
 from alien import Alien
 from time import sleep
 
-
 def check_events(ai_settings, screen, ship, bullets):
     # Отслеживание событий клавиатуры
     for event in pygame.event.get():
@@ -35,13 +34,15 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def update_screen(ai_settings, screen,stats, ship, aliens, bullets,play_button):
     screen.fill(ai_settings.bg_color)
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
 
     aliens.draw(screen)
+    if not stats.game_active:
+        play_button.draw_button()
     # Отображение последнего прорисованного экрана
     pygame.display.flip()
 
